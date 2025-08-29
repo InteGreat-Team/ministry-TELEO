7775acb0bb6202e4d233e4c1fa93c356f1dc8e9a
arawenhailyn <arawen.hailyn@gmail.com>
2025-08-29 22:44:25 +0800

    fixed the error

--- lib/2/c2eventscreation/c2s6_1caeventcreation.dart
+++ lib/2/c2eventscreation/c2s6_1caeventcreation.dart
@@ -155,6 +155,7 @@ class _EventRegistrationFormScreenState extends State<EventRegistrationFormScree
               _showInfoSnackbar('Terms and conditions viewed but not accepted');
             }
           },
+           event: widget.event,
         ),
       ),
     );
